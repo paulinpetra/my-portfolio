b716265fe28e677a763c28229768d3bbc700e539
paulinpetra <117924207+paulinpetra@users.noreply.github.com>
2024-04-17 10:26:32 +0200

    added more projects to my work

--- src/app/components/My_Work/MyWork.jsx
+++ src/app/components/My_Work/MyWork.jsx
@@ -8,7 +8,48 @@ const MyWork = () => {
           <div className="flex justify-center items-center">
             <h1 className="text-[#151b3f] text-3xl font-semibold">My Work</h1>
           </div>
+
+          <div className="bg-white flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
+            <h1 className="text-[#151b3f] text-3xl font-semibold">
+              Radio player
+            </h1>
+            <label className="text-[#151b3f]  text-base">
+              HTML, Tailwind CSS, JS, React
+            </label>
+            <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
+            <p className="text-[#151b3f]  text-base text-left px-2">
+              A Radio player using Sveriges Radio API.
+            </p>
+            <a
+              href="https://radio-player-react-sandy.vercel.app/"
+              className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
+            >
+              <label>See More</label>
+            </a>
+          </div>
+
           <div className="flex flex-col lg:flex-row gap-12">
+            <div className="bg-white flex flex-col justify-center items-center py-8 px-4 gap-4 opacity-95">
+              <h1 className="text-[#151b3f] text-3xl font-semibold">
+                Clocks and Timers
+              </h1>
+              <label className="text-[#151b3f] text-base">
+                HTML, Tailwind CSS, JS, React
+              </label>
+              <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
+              <p className="text-[#151b3f] text-base text-left px-2">
+                Clock-component that uses a prop to show the city title of the
+                clock and another prop to show time at that timezone.
+                Timer-components with different starting time props.
+              </p>
+              <a
+                href="https://clock-nine-iota.vercel.app/"
+                className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
+              >
+                <label>See More</label>
+              </a>
+            </div>
+
             <div className="bg-white flex flex-col justify-center items-center py-8 px-4 gap-4 opacity-95">
               <h1 className="text-[#151b3f] text-3xl font-semibold">
                 Shopping Cart App
@@ -31,18 +72,38 @@ const MyWork = () => {
 
             <div className="bg-white flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
               <h1 className="text-[#151b3f] text-3xl font-semibold">
-                Todo-list App
+                Zoom redesign
               </h1>
               <label className="text-[#151b3f]  text-base">
                 HTML, Tailwind CSS, JS, React
               </label>
               <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
               <p className="text-[#151b3f]  text-base text-left px-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                A redesign of the front page of zoom.us to make it more
+                user-friendly and and so that it follows design rules.
               </p>
               <a
-                href="https://redux-todo-ten-pied.vercel.app/"
+                href="https://zoom-redesign-phi.vercel.app/"
+                className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
+              >
+                <label>See More</label>
+              </a>
+            </div>
+
+            <div className="bg-white flex flex-col justify-center items-center py-8 px-4 gap-4 opacity-95">
+              <h1 className="text-[#151b3f] text-3xl font-semibold">
+                React Component Library
+              </h1>
+              <label className="text-[#151b3f] text-base">
+                React, Tailwind CSS
+              </label>
+              <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
+              <p className="text-[#151b3f] text-base text-left px-2">
+                Reusuable components like buttons, alerts, badges and cards. To
+                be be used on many platforms and websites.
+              </p>
+              <a
+                href="https://component-library-self-theta.vercel.app/"
                 className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
               >
                 <label>See More</label>
