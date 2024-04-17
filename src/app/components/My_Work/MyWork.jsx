1dc3028a39aef9479c9c9692f06c91c53ab4ec0c
paulinpetra <117924207+paulinpetra@users.noreply.github.com>
2024-04-17 11:47:10 +0200

    improved layout on work-cards

--- src/app/components/My_Work/MyWork.jsx
+++ src/app/components/My_Work/MyWork.jsx
@@ -3,110 +3,145 @@ import React from "react";
 const MyWork = () => {
   return (
     <>
-      <section id="services">
-        <div className="bg-[#ffdebc] flex flex-col justify-center items-center font-Inconsolata gap-12 py-52">
-          <div className="flex justify-center items-center">
-            <h1 className="text-[#151b3f] text-3xl font-semibold">My Work</h1>
-          </div>
+      <section
+        id="services"
+        className="bg-[#ffdebc] flex flex-col justify-center items-center font-Inconsolata gap-12 py-52"
+      >
+        <div className="flex justify-center items-center">
+          <h1 className="text-[#151b3f] text-3xl font-semibold">My Work</h1>
+        </div>
 
-          <div className="bg-white flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
-            <h1 className="text-[#151b3f] text-3xl font-semibold">
-              Zoom redesign
-            </h1>
-            <label className="text-[#151b3f]  text-base">
-              HTML, Tailwind CSS, JS, React
-            </label>
-            <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
-            <p className="text-[#151b3f]  text-base text-left px-2">
-              A redesign of the front page of zoom.us to make it more
-              user-friendly and and so that it follows design rules.
-            </p>
-            <a
-              href="https://zoom-redesign-phi.vercel.app/"
-              className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
-            >
-              <label>See More</label>
-            </a>
-          </div>
-
-          <div className="bg-white flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
-            <h1 className="text-[#151b3f] text-3xl font-semibold">
-              Radio player
-            </h1>
-            <label className="text-[#151b3f]  text-base">
-              HTML, Tailwind CSS, JS, React
-            </label>
-            <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
-            <p className="text-[#151b3f]  text-base text-left px-2">
-              A Radio player using Sveriges Radio API.
-            </p>
-            <a
-              href="https://radio-player-react-sandy.vercel.app/"
-              className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
-            >
-              <label>See More</label>
-            </a>
-          </div>
-
-          <div className="flex flex-col lg:flex-row gap-12">
-            <div className="bg-white flex flex-col justify-center items-center py-8 px-4 gap-4 opacity-95">
-              <h1 className="text-[#151b3f] text-3xl font-semibold">
-                Clocks and Timers
-              </h1>
-              <label className="text-[#151b3f] text-base">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-8 gap-y-4">
+          {/* Zoom Redesign Card */}
+          <div className="bg-white flex flex-col justify-between items-center py-8 px-4 gap-4 opacity-95 w-full sm:w-96 md:w-full lg:w-96 xl:w-full rounded-lg">
+            <div>
+              <h2 className="text-[#151b3f] text-3xl font-semibold text-center">
+                Zoom Redesign
+              </h2>
+              <p className="text-[#151b3f] text-base font-meium leading-6 text-center py-2">
                 HTML, Tailwind CSS, JS, React
-              </label>
-              <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
-              <p className="text-[#151b3f] text-base text-left px-2">
+              </p>
+              <div className="underline w-full border border-solid border-[#88a296]"></div>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-left px-2 py-4">
+                A redesign of the front page of zoom.us to make it more
+                user-friendly and so that it follows design rules.
+              </p>
+            </div>
+            <div className="flex justify-end">
+              <a
+                href="https://zoom-redesign-phi.vercel.app/"
+                className="cursor-pointer"
+              >
+                <button className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold">
+                  See More
+                </button>
+              </a>
+            </div>
+          </div>
+
+          {/* Radio Player Card */}
+          <div className="bg-white flex flex-col justify-between items-center py-8 px-4 gap-4 opacity-95 w-full sm:w-96 md:w-full lg:w-96 xl:w-full rounded-lg">
+            <div>
+              <h2 className="text-[#151b3f] text-3xl font-semibold text-center">
+                Radio Player
+              </h2>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-center py-2">
+                HTML, Tailwind CSS, JS, React
+              </p>
+              <div className="underline w-full border border-solid border-[#88a296]"></div>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-left px-2 py-4">
+                A Radio player using Sveriges Radio API.
+              </p>
+            </div>
+            <div className="flex justify-end">
+              <a
+                href="https://radio-player-react-sandy.vercel.app/"
+                className="cursor-pointer"
+              >
+                <button className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold">
+                  See More
+                </button>
+              </a>
+            </div>
+          </div>
+
+          {/* Clocks and Timers Card */}
+          <div className="bg-white flex flex-col justify-between items-center py-8 px-4 gap-4 opacity-95 w-full sm:w-96 md:w-full lg:w-96 xl:w-full rounded-lg">
+            <div>
+              <h2 className="text-[#151b3f] text-3xl font-semibold text-center">
+                Clocks and Timers
+              </h2>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-center py-2">
+                HTML, Tailwind CSS, JS, React
+              </p>
+              <div className="underline w-full border border-solid border-[#88a296]"></div>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-left px-2 py-4">
                 Clock-component that uses a prop to show the city title of the
                 clock and another prop to show time at that timezone.
                 Timer-components with different starting time props.
               </p>
+            </div>
+            <div className="flex justify-end">
               <a
                 href="https://clock-nine-iota.vercel.app/"
-                className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
+                className="cursor-pointer"
               >
-                <label>See More</label>
+                <button className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold">
+                  See More
+                </button>
               </a>
             </div>
+          </div>
 
-            <div className="bg-white flex flex-col justify-center items-center py-8 px-4 gap-4 opacity-95">
-              <h1 className="text-[#151b3f] text-3xl font-semibold">
+          {/* Shopping Cart App Card */}
+          <div className="bg-white flex flex-col justify-between items-center py-8 px-4 gap-4 opacity-95 w-full sm:w-96 md:w-full lg:w-96 xl:w-full rounded-lg">
+            <div>
+              <h2 className="text-[#151b3f] text-3xl font-semibold text-center">
                 Shopping Cart App
-              </h1>
-              <label className="text-[#151b3f] text-base">
+              </h2>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-center py-2">
                 HTML, CSS, JS, Firebase
-              </label>
-              <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
-              <p className="text-[#151b3f] text-base text-left px-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
+              <div className="underline w-full border border-solid border-[#88a296]"></div>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-left px-2 py-4">
+                Moolang shopping cart app using Firebase.
+              </p>
+            </div>
+            <div className="flex justify-end">
               <a
                 href="https://moolang-shopping-cart-app.vercel.app/"
-                className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
+                className="cursor-pointer"
               >
-                <label>See More</label>
+                <button className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold">
+                  See More
+                </button>
               </a>
             </div>
+          </div>
 
-            <div className="bg-white flex flex-col justify-center items-center py-8 px-4 gap-4 opacity-95">
-              <h1 className="text-[#151b3f] text-3xl font-semibold">
+          {/* React Component Library Card */}
+          <div className="bg-white flex flex-col justify-between items-center py-8 px-4 gap-4 opacity-95 w-full sm:w-96 md:w-full lg:w-96 xl:w-full rounded-lg">
+            <div>
+              <h2 className="text-[#151b3f] text-3xl font-semibold text-center">
                 React Component Library
-              </h1>
-              <label className="text-[#151b3f] text-base">
+              </h2>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-center py-2">
                 React, Tailwind CSS
-              </label>
-              <span className="underline w-[200px] border border-solid border-[#88a296]"></span>
-              <p className="text-[#151b3f] text-base text-left px-2">
-                Reusuable components like buttons, alerts, badges and cards. To
-                be be used on many platforms and websites.
               </p>
+              <div className="underline w-full border border-solid border-[#88a296]"></div>
+              <p className="text-[#151b3f] text-base font-medium leading-6 text-left px-2 py-4">
+                Reusable components like buttons, alerts, badges, and cards. To
+                be used on many platforms and websites.
+              </p>
+            </div>
+            <div className="flex justify-end">
               <a
                 href="https://component-library-self-theta.vercel.app/"
-                className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold"
+                className="cursor-pointer"
               >
-                <label>See More</label>
+                <button className="bg-[#88a296] py-2 px-2 rounded-md text-white hover:font-bold">
+                  See More
+                </button>
               </a>
             </div>
           </div>
